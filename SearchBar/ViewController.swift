b24ce344161295eb8ea9710d8e78ee1f0adee1f8
John DongHyun Kim <zapping0320@gmail.com>
2019-07-05 06:56:28 +0900

    add tableview default function

--- SearchBar/ViewController.swift
+++ SearchBar/ViewController.swift
@@ -8,16 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
+   
+    var wordList = ["all", "blue", "sky", "hello",
+                    "number", "marvel", "spiderman",
+                    "친구", "사랑", "재범", "몸매"]
+    
+    var filterWordList:[String] = []
+    var isFiltered = false
+    
 
+    @IBOutlet weak var searchTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let searchVC = UISearchController(searchResultsController: nil)
         
         self.navigationItem.searchController = searchVC
+        
+        self.navigationItem.hidesSearchBarWhenScrolling = false
+        
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch isFiltered {
+        case true:
+            return filterWordList.count
+        case false:
+            return wordList.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if isFiltered {
+            return filteredWordListCell
+        }else {
+            return workListCell
+        }
+        
+    }
+}
