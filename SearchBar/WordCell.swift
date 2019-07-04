20d7bd0c221b6b0a5dead094213995315c73ac5d
John DongHyun Kim <zapping0320@gmail.com>
2019-07-05 07:08:25 +0900

    add word cell class for tableview

---
+++ SearchBar/WordCell.swift
@@ -0,0 +1,25 @@
+//
+//  WordCell.swift
+//  SearchBar
+//
+//  Created by 김동현 on 05/07/2019.
+//  Copyright © 2019 John Kim. All rights reserved.
+//
+
+import UIKit
+
+class WordCell: UITableViewCell {
+
+    @IBOutlet weak var wordLabel: NSLayoutConstraint!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
